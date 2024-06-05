303a106c1478c431a5948e9bcff558ef001e5647
yasubei <n8anko10mamenoki@gmail.com>
2024-06-05 15:55:10 +0900

    中間テストの解答を配信

---
+++ 06他のPythonファイルの関数を使う/mymath.py
@@ -0,0 +1,11 @@
+
+import random
+
+# マイルをメートルに変換するシンプルなユーザー定義関数の例
+def mile2meter(mile):
+    meter = mile * 1609.344
+    return meter
+
+# ヤードをメートルに変換するシンプルなユーザー定義関数の例
+def yard2meter(yard):
+    return yard * 0.9144
